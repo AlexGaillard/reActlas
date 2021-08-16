d9f538cd92916d916d625bfcef43b5d4d45fa4fc
Alex Gaillard <agaillard88@gmail.com>
2021-08-16 15:48:09 -0400

    Implemented basic react router function

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { getAllCountries, getRegionCountries, searchCountries } from '../requests.js';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import CountryCard from './CountryCard.jsx';
 import Header from './Header.jsx';
 import Search from './Search.jsx';
@@ -38,18 +39,32 @@ const App = () => {
   };
 
   return(
-    <div>
-      <Header />
-      <div id="filter-search">
-        <Search searchResults={searchResults} />
-        <Filter filterResults={filterResults} />
+    <Router>
+      <div>
+      <Link to="/">Home</Link>
+      <Link to="/test">Test</Link>
+        <Header />
+        <Switch>
+
+          <Route path="/test">
+            <h1>TEST</h1>
+          </Route>
+
+          <Route path="/">
+            <div id="filter-search">
+              <Search searchResults={searchResults} />
+              <Filter filterResults={filterResults} />
+            </div>
+            <div id="countries">
+              { countries.length ? countries.map(country => {
+                return <CountryCard key={country.name} countryData={country}/>
+              }) : <p>Loading...</p>}
+            </div>
+          </Route>
+
+        </Switch>
       </div>
-      <div id="countries">
-        { countries.length ? countries.map(country => {
-          return <CountryCard key={country.name} countryData={country}/>
-        }) : <p>Loading...</p>}
-      </div>
-    </div>
+    </Router>
   );
 
 }
