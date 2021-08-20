a576e57bce192f701ddad4f15af8b867b6687118
Alex Gaillard <agaillard88@gmail.com>
2021-08-20 15:06:21 -0400

    Added dark mode icon change

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMoon } from '@fortawesome/free-regular-svg-icons'
+import { faMoon as fasMoon } from '@fortawesome/free-solid-svg-icons'
 
 const Nav = ({ darkMode, setDarkMode }) => {
 
@@ -19,7 +20,7 @@ const Nav = ({ darkMode, setDarkMode }) => {
   return(
     <nav id="header">
       <h1>Where in the world?</h1>
-      <p onClick={handleClick}>{<FontAwesomeIcon icon={faMoon} />}Dark Mode</p>
+      <p onClick={handleClick}>{<FontAwesomeIcon icon={darkMode ? fasMoon : faMoon} />}Dark Mode</p>
     </nav>
   );
 
