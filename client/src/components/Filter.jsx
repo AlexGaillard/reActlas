e44044edeb78d3cd05ee4a8b95c279a8431b2f7d
Alex Gaillard <agaillard88@gmail.com>
2021-08-21 12:57:45 -0400

    Added semi-styled select with functionality

--- client/src/components/Filter.jsx
+++ client/src/components/Filter.jsx
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
 const Filter = ({ filterString, setFilterString, resetFilter }) => {
 
-  const handleChange = (e) => {
-    setFilterString(e.target.value);
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const toggling = () => setIsOpen(!isOpen);
+
+  const handleClick = (e) => {
+    let value = e.target.innerText;
+    setSelectedOption(value);
+    setFilterString(value.toLowerCase());
+    setIsOpen(false);
   };
+
+  const options = ["Africa", "Americas", "Asia", "Europe", "Oceania"];
 
   return(
     <>
-    <div id="filter">
+      <div className="drop-down-container">
+        <div className="drop-down-header" onClick={toggling}>{selectedOption || "Filter by region..."}{filterString && <FontAwesomeIcon icon={faTimesCircle} onClick={resetFilter} />}</div>
+        {isOpen && (
+        <div className="drop-down-list-container">
+          <ul className="drop-down-list">
+          {options.map(option => (
+            <li className="list-item" onClick={handleClick} key={Math.random()}>
+                {option}
+            </li>
+          ))}
+          </ul>
+        </div>
+        )}
+      </div>
+    </>
+  );
+
+};
+
+export default Filter;
+
+    {/* <div id="filter">
       {filterString && <FontAwesomeIcon icon={faTimesCircle} onClick={resetFilter} />}
       <select name="filter" onChange={handleChange} value={filterString}>
         <option hidden>Filter by Region</option>
@@ -20,10 +51,4 @@ const Filter = ({ filterString, setFilterString, resetFilter }) => {
         <option value="europe">Europe</option>
         <option value="oceania">Oceania</option>
       </select>
-    </div>
-    </>
-  );
-
-};
-
-export default Filter;
+    </div> */}
