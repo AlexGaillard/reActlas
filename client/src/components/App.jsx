ce567f79f97df139a79f1e9555ceb68b2fc9e2e0
Alex Gaillard <agaillard88@gmail.com>
2021-08-23 14:15:46 -0400

    Removed unused commented line

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect, lazy, Suspense } from 'react';
 import { getAllCountries } from '../requests.js';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Nav from './Nav.jsx';
-// import CountryDetails from './CountryDetails.jsx';
 import Search from './Search.jsx';
 import Filter from './Filter.jsx';
 
