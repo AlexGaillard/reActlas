d589677ad536d8572c3df55db509de8fc766fd22
Alex Gaillard <agaillard88@gmail.com>
2021-08-26 13:15:34 -0400

    Moved suspense higher

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, lazy, Suspense } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { useState, useEffect, lazy } from "react";
+import { Switch, Route } from "react-router-dom";
 import { Helmet } from "react-helmet";
 import { getAllCountries } from "../requests.js";
 import Nav from "./Nav.jsx";
@@ -25,16 +25,12 @@ const App = () => {
       <Helmet bodyAttributes={darkMode && { class: "dark" }} />
       <Nav darkMode={darkMode} setDarkMode={setDarkMode} />
       <div id="container">
-        <Suspense fallback={null}>
-          <Router>
-            <Switch>
-              <Route path="/:id" component={CountryDetails} />
-              <Route path="/">
-                <Homepage countries={countries} displayed={displayed} setDisplayed={setDisplayed} darkMode={darkMode} />
-              </Route>
-            </Switch>
-          </Router>
-        </Suspense>
+        <Switch>
+          <Route path="/:id" component={CountryDetails} />
+          <Route path="/">
+            <Homepage countries={countries} displayed={displayed} setDisplayed={setDisplayed} darkMode={darkMode} />
+          </Route>
+        </Switch>
       </div>
     </>
   );
