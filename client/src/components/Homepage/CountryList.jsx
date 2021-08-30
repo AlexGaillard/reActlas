49fe75d4005bda5e57a4fbc0b1af905d248dbb13
Alex Gaillard <agaillard88@gmail.com>
2021-08-30 09:04:25 -0400

    Added delay to react router with scroll to top before change

--- client/src/components/Homepage/CountryList.jsx
+++ client/src/components/Homepage/CountryList.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import CountryCard from "../Homepage/CountryCard.jsx";
+import DelayLink from 'react-delay-link';
 
 const CountryList = ({ displayed }) => {
   return displayed.map((country) => {
+
+    const handleClick = () => {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    };
+
     return (
-      <Link
+      <DelayLink
+        delay={500}
+        clickAction={handleClick}
         to={{ pathname: `/${country.name}`, state: { country } }}
         key={country.alpha3Code}
       >
         <CountryCard key={country.name} countryData={country} />
-      </Link>
+      </DelayLink>
     );
   });
 };
